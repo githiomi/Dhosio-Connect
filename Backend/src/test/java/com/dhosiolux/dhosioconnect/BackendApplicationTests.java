8ea198b08d58de73d23c3b09a27f432bbf2015b9
Daniel Githiomi <danielgithiomi@gmail.com>
2023-06-28 02:28:42 +0400

    Initial project commit with spring backend

---
+++ Backend/src/test/java/com/dhosiolux/dhosioconnect/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhosiolux.dhosioconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
